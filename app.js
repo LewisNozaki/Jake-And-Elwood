22813f6621341fdb4c6024208419f59fd06da5f5
Lewis Nozaki <lhnozaki@outlook.com>
2021-06-03 15:32:26 -1000

    updates

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 let closeBtn = document.getElementsByClassName("close-nav")[0];
 
+let openBtn = document.getElementsByClassName("open-nav")[0];
+
 let navigation = document.getElementsByClassName("nav")[0];
 
 const closeNav = (e) => {
-  console.log("works");
   navigation.classList.remove("navigation-open");
+  document.body.classList.remove("modal-open");
 };
 
 closeBtn.addEventListener("click", closeNav);
+
+const openNav = (e) => {
+  navigation.classList.add("navigation-open");
+  document.body.classList.add("modal-open");
+};
+
+openBtn.addEventListener("click", openNav);
